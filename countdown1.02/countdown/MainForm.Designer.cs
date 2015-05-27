e04fee704d3b4026c3a7d8f2a4822526c925fd8a
文月 <requiem.for.memories@gmail.com>
2015-05-27 10:16:44 +0800

    1.02
    是說暫停有沒有更好的辦法QAQ"

---
+++ countdown1.02/countdown/MainForm.Designer.cs
@@ -0,0 +1,249 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fungchu
+ * Date: 2014/11/8
+ * Time: 下午 05:51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace countdown
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.minlabel = new System.Windows.Forms.Label();
+			this.seclabel = new System.Windows.Forms.Label();
+			this.minbox = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.secbox = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(29, 137);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(129, 36);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "現在還剩";
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label2.Location = new System.Drawing.Point(275, 256);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 34);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "分";
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.Location = new System.Drawing.Point(515, 256);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(125, 34);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "秒喔!!";
+			// 
+			// minlabel
+			// 
+			this.minlabel.BackColor = System.Drawing.Color.Transparent;
+			this.minlabel.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.minlabel.Location = new System.Drawing.Point(100, 214);
+			this.minlabel.Name = "minlabel";
+			this.minlabel.Size = new System.Drawing.Size(149, 91);
+			this.minlabel.TabIndex = 3;
+			this.minlabel.Text = "0";
+			// 
+			// seclabel
+			// 
+			this.seclabel.BackColor = System.Drawing.Color.Transparent;
+			this.seclabel.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.seclabel.Location = new System.Drawing.Point(352, 214);
+			this.seclabel.Name = "seclabel";
+			this.seclabel.Size = new System.Drawing.Size(157, 91);
+			this.seclabel.TabIndex = 4;
+			this.seclabel.Text = "0";
+			// 
+			// minbox
+			// 
+			this.minbox.Location = new System.Drawing.Point(83, 360);
+			this.minbox.Name = "minbox";
+			this.minbox.Size = new System.Drawing.Size(72, 25);
+			this.minbox.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label4.Location = new System.Drawing.Point(26, 359);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(58, 36);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "倒數";
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label5.Location = new System.Drawing.Point(161, 359);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(44, 36);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "分";
+			// 
+			// secbox
+			// 
+			this.secbox.Location = new System.Drawing.Point(197, 360);
+			this.secbox.Name = "secbox";
+			this.secbox.Size = new System.Drawing.Size(72, 25);
+			this.secbox.TabIndex = 8;
+			// 
+			// label6
+			// 
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label6.Location = new System.Drawing.Point(284, 359);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(44, 36);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "秒";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button1.Location = new System.Drawing.Point(321, 353);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 33);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "確定";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button2.Location = new System.Drawing.Point(411, 353);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 33);
+			this.button2.TabIndex = 11;
+			this.button2.Text = "暫停";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Visible = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button3.Location = new System.Drawing.Point(501, 353);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 33);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "重來";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Visible = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label7
+			// 
+			this.label7.BackColor = System.Drawing.Color.Transparent;
+			this.label7.Location = new System.Drawing.Point(501, 416);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(165, 23);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "10130113 製>w<";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(121, 31);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(493, 62);
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(694, 448);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.secbox);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.minbox);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.seclabel);
+			this.Controls.Add(this.minlabel);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "countdown";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox secbox;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox minbox;
+		private System.Windows.Forms.Label seclabel;
+		private System.Windows.Forms.Label minlabel;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ countdown1.02/countdown/MainForm.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fungchu
+ * Date: 2014/11/8
+ * Time: 下午 05:51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace countdown
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		int mini,seci,minp,secp,pause=0;
+		System.Media.SoundPlayer sp = new System.Media.SoundPlayer(Application.StartupPath+"\\music.wav");
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void count(int min,int sec)
+		{
+			if(pause==0)
+			{
+				button2.Visible=true;
+			}
+			button1.Enabled=false;
+			for(mini=min;mini>=0;mini--)
+			{
+				//MessageBox.Show(Convert.ToString(mini));  //debug
+					
+				minlabel.Text=Convert.ToString(mini);
+				for(seci=sec;seci>=0;seci--)
+				{
+					//MessageBox.Show(Convert.ToString(seci));  //debug
+					seclabel.Text=Convert.ToString(seci);
+					Application.DoEvents();
+					Thread.Sleep(1000);
+
+				}
+				sec=59;
+				}
+			if(pause==0 || pause==2)
+				{
+					button3.Visible=false;
+					button2.Visible=false;
+					button1.Enabled=true;
+				}
+			
+			if(pause==0)
+				{
+					sp.Play();
+					MessageBox.Show("時間到了喔!!","鈴鈴鈴!!");	
+				}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{	
+			
+			int min,sec;
+			try
+			{	
+				if(minbox.Text=="")
+				{
+					min=0;
+				}
+				else
+				{
+					min=Convert.ToInt32(minbox.Text);
+				}
+				if(secbox.Text=="")
+				{
+					sec=0;
+				}
+				else
+				{
+					sec=Convert.ToInt32(secbox.Text);
+				}
+				if(sec>=60)
+				{
+					MessageBox.Show("懶惰鬼!!秒數不會自己換算一下喔....","Hey You!!!!");
+					min+=sec/60;
+					sec%=60;
+				}
+				count(min,sec);
+				
+				
+								
+			} 
+			catch (Exception)
+			{
+				
+				MessageBox.Show("哎哎哎!!!輸入的格式有誤喔","哎哎哎!!!");
+				button1.Enabled=true;
+				button2.Visible=false;				
+				//throw;
+			}
+			
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			if(pause==0)
+			{
+				minp=mini;
+				secp=seci;
+				mini=0;
+				seci=0;
+				pause=1;
+				button2.Text="繼續";
+				button3.Visible=true;
+				button1.Enabled=false;
+			}
+			else
+			{
+				mini=minp;
+				seci=secp;
+				pause=0;
+				button2.Text="暫停";
+				button3.Visible=false;
+				count(minp,secp);
+			}
+			
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			pause=2;
+			mini=0;
+			seci=0;
+			minlabel.Text=Convert.ToString(0);
+			seclabel.Text=Convert.ToString(0);
+			button3.Visible=false;
+			button2.Visible=false;
+			Application.DoEvents();			
+			button2.Text="暫停";
+			pause=0;
+			button1.Enabled=true;
+			
+			
+		}
+	}
+}
